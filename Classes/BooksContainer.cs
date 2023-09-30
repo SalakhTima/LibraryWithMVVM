bcd925b11e522b188042e1da772731143f234d5a
User <User@DESKTOP-MANCTJR>
2023-09-30 21:30:36 +0700

    Добавьте файлы проекта.

---
+++ Classes/BooksContainer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TaskLibrary.Abstraction;
+
+namespace TaskLibrary.Classes;
+
+public class BooksContainer : VariableBase<Book>
+{
+    public override List<Book> Container { get; protected set; } = new();
+    public override void Add(Book book) => Container.Add(book);
+    public override void Remove(Book book) => Container.Remove(book);
+    public override Book? Find(int identifier)
+    {
+        return Container.FirstOrDefault(r => r.Article == identifier);
+    }
+}
---
+++ Classes/ReadersContainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TaskLibrary.Abstraction;
+
+namespace TaskLibrary.Classes;
+
+public class ReadersContainer : VariableBase<Reader>
+{
+    public override List<Reader> Container { get; protected set; } = new();
+    public override void Add(Reader reader) => Container.Add(reader);
+    public override void Remove(Reader reader) => Container.Remove(reader);
+    public override Reader? Find(int identifier)
+    {
+        return Container.FirstOrDefault(r => r.Id == identifier);
+    }
+}
